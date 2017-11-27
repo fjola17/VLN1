dc9c4b603e807b29f2c6756bca59caae6f61089e
fjola17 <34029285+fjola17@users.noreply.github.com>
2017-11-27 13:31:47 +0000

    Add files via upload

---
+++ SuperHero.h
@@ -0,0 +1,29 @@
+#ifndef SUPERHERO_H
+#define SUPERHERO_H
+
+#include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+class SuperHero
+{
+    public:
+        SuperHero();
+        SuperHero(string name,int age, char power);
+        string set_name(string nam);
+        int set_age(int ag);
+        char set_super_power(char powah);
+        friend ostream& operator << (ostream& out, SuperHero& hero);
+        friend istream& operator >> (istream& in, SuperHero& hero);
+
+        virtual ~SuperHero();
+
+    private:
+        string name;
+        int age;
+        char power;
+};
+
+#endif // SUPERHERO_H
