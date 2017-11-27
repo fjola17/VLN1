49526f2137637bfc2d74d729e8ffd90298a2283f
halldorvalberg <34029264+halldorvalberg@users.noreply.github.com>
2017-11-27 14:09:22 +0000

    directory change

---
+++ Textaskrar/i/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    string str;
+    ifstream fin;
+    char input = 0;
+    fin.open("Datafile.txt");
+
+    do{
+    for(int i = 0; i < 10; i++){
+       if(fin.eof()== false){
+        getline(fin, str);
+        cout << str << endl;
+        }
+        else
+            break;
+    }
+
+    if(fin.eof()== true){
+        break;
+    }
+
+
+
+    do{
+        cout << "Do you wish to continue (y/n)?";
+        cin >> input;
+
+        if(input != 'n' && input != 'y'){
+            cout << "Invalid input!" << endl;
+        }
+        else{
+            break;
+        }
+
+    }while(1);
+
+    }while(input != 'n');
+
+    fin.close();
+    return 0;
+}
