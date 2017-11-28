ca7051b94d9318886f0ba11ea4a2faff92a1248c
fjola17 <34029285+fjola17@users.noreply.github.com>
2017-11-28 16:15:13 +0000

    .
    ekki kóði eins og var verið að byðja um

--- SuperTeamA/main.cpp
+++ SuperTeamA/main.cpp
@@ -8,37 +8,9 @@ using namespace std;
 int main()
 {
     SuperHero hero;
-  /*  string name;
-    int age;
-    char power, input;
-   */ cout << "Please enter hero name, age and power: ";
+    cout << "Please enter hero name, age and power: ";
     cin >> hero;
-/*
-    ofstream fout;
-    fout.open("heroes.txt", ios::app);
-    if(fout.is_open()){
-        fout << hero;
-        fout.close();
-    }
-
-    ifstream fin;
-    fin.open("heroes.txt");
-    if(fin.is_open()){
-        fin >> hero;
-        }
-    else{
-        cout << "File does not exist" << endl;
-    }
-string str;
-char input;
-    do{
-        getline(fin, str);
-        cout << str;
-        cout << "another hero?" << endl;
-        cin >> input;
-    }while (input == 'y');
-    fin.close();
-*/
     cout << hero;
+    //not copying things to file
     return 0;
 }
