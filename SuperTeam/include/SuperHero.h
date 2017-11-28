80a3862639002c2395bb9a1f2836be1af36f1a58
fjola17 <34029285+fjola17@users.noreply.github.com>
2017-11-28 13:29:28 +0000

    .
    .

---
+++ SuperTeam/include/SuperHero.h
@@ -0,0 +1,26 @@
+#ifndef SUPERHERO_H
+#define SUPERHERO_H
+
+#include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+class SuperHero
+{
+    public:
+        SuperHero();
+        SuperHero(string name,int age, char power);
+        friend ostream& operator << (ostream& out, SuperHero& hero);
+        friend istream& operator >> (istream& in, SuperHero& hero);
+
+        virtual ~SuperHero();
+
+    private:
+        string name;
+        int age;
+        char power;
+};
+
+#endif // SUPERHERO_H
