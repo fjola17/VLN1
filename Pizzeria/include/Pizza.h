dc9b82ab76207ff6a1fe150e73fc4ad9302ae7f0
Danilkant <Danilkant@hotmail.com>
2017-11-29 13:41:12 +0000

    Fixed github

--- Pizzeria/include/Pizza.h
+++ Pizzeria/include/Pizza.h
@@ -13,22 +13,9 @@ class Pizza
     public:
         Pizza();
         virtual ~Pizza();
-<<<<<<< HEAD
 
-        int getsize(){
-            return this->size;
-        }
-        double getprize(){
-            return this->prize;
-        }
-=======
         int getsize();
         double getprize();
-        //need to put something about overloading operators here???
-        friend istream& operator >> (istream& in, Pizza& pizza);
-        friend ostream& operator << (ostream& out, Pizza& pizza);
-
->>>>>>> b6e4f0dcfec7a8441a136c5dfc001bf159f272e6
 
     protected:
 
@@ -36,11 +23,7 @@ class Pizza
         int size;
         double prize;
         string name;
-<<<<<<< HEAD
         vector<Toppings> topArr;
-=======
-        Toppings* topping;
->>>>>>> b6e4f0dcfec7a8441a136c5dfc001bf159f272e6
 };
 
 #endif // PIZZA_H
--- Pizzeria/src/Pizza.cpp
+++ Pizzeria/src/Pizza.cpp
@@ -6,21 +6,14 @@ Pizza::Pizza()
     this->prize = 0;
     this->name = "";
 }
-<<<<<<< HEAD
-=======
-Pizza::Pizza(int value){
-    topping* = new topping = [amountOfToppings]; //dynamic array of toppings, might need to change it to vectors
-}
 
-int getsize(){
+int Pizza::getsize(){
     return size;
 }
 double getprize(){
     return prize;
 }
->>>>>>> b6e4f0dcfec7a8441a136c5dfc001bf159f272e6
 
 Pizza::~Pizza()
 {
-    delete[] topping;
 }
