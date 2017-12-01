e5a6277270db3fc77a8c48bfe9e1b9933d642f36
fjola17 <34029285+fjola17@users.noreply.github.com>
2017-12-01 15:46:08 +0000

    Salesman menu added

--- Paelingar/Petsa/Controller/SalesmanMenuController.cpp
+++ Paelingar/Petsa/Controller/SalesmanMenuController.cpp
@@ -2,10 +2,43 @@
 
 SalesmanMenuController::SalesmanMenuController()
 {
-    //ctor
+    clearScrean();
+    displayHeader();
+    init();
 }
 
 SalesmanMenuController::~SalesmanMenuController()
 {
     //dtor
+}
+void SalesmanMenuController::SalesmanMenu(){
+    cout << "Salesman menu" << endl << endl;
+    cout << "What do you want to do? " << endl;
+    cout << "d: Display all pizzas" << endl;
+    cout << "c: Checkout a pizza order" << endl;
+    cout << "q: Go back to main menu" << endl;
+}
+
+void SalesmanMenuController::init(){
+    string location, inputstring;
+    SalesmanMenu();
+    cin >> inputstring;
+    do{
+        Bakermenu();
+        cin >> input;
+    }while(input != "d" && input != "m" && input != "q");
+    if (input == "d"){
+        location = getLocation();
+        cout << "Displaying pizzas from your location" << endl;
+        cout << "This doesn't do anything yet" << endl;
+        // displayPizzas(location); //útfæri síðar
+    }
+    else if (input == "m"){
+        location = getLocation();
+        cout << endl << "Select which order you want to process?" << endl;
+        selectPizza(); //fer inn í annað fall og velur hvaða pizza um ræðir og merkja hvort hún sé tilbúin eða í vinnslu
+    }
+    else if (input == "q"){
+
+    }
 }
--- Paelingar/Petsa/Controller/SalesmanMenuController.h
+++ Paelingar/Petsa/Controller/SalesmanMenuController.h
@@ -1,16 +1,26 @@
 #ifndef SALESMANMENUCOTROLLER_H
 #define SALESMANMENUCOTROLLER_H
 
+#include <iostream>
+#include <string>
+#include "GlobalTools.h"
+#include "GlobalView.h"
+
+using namespace std;
+using namespace GlobalTools;
+using namespace GlobalView;
 
 class SalesmanMenuController
 {
     public:
         SalesmanMenuController();
         virtual ~SalesmanMenuController();
+        void SalesmanMenu();
 
     protected:
 
     private:
+        void init();
 };
 
 #endif // SALESMANMENUCOTROLLER_H
