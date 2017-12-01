846621081a8198735ec291b1cfa65368a7184546
fjola17 <34029285+fjola17@users.noreply.github.com>
2017-12-01 13:21:27 +0000

    bla

--- Paelingar/Petsa/Controller/AdminMenuController.cpp
+++ Paelingar/Petsa/Controller/AdminMenuController.cpp
@@ -1,11 +1,53 @@
-#include "AdminMenuController .h"
+#include "AdminMenuController.h"
 
 AdminMenuController ::AdminMenuController ()
 {
-    //ctor
+    displayHeader();
+    displayAdminMenu();
+    init();
 }
 
 AdminMenuController ::~AdminMenuController ()
 {
     //dtor
+}
+void AdminMenuController::displayAdminMenu(){
+    cout << "What do you want to do?" << endl;
+    cout << "d: Display your favorite pizza" << endl;
+    cout << "p: Create a new pizza" << endl;
+    cout << "t: Create a topping for your pizza" << endl;
+    cout << "d: Display all available pizzas" << endl;
+    cout << "q: Exit application" << endl;
+}
+
+void AdminMenuController::init(){
+     string input;
+
+    do{
+        displayAdminMenu();
+        cin >> input;
+
+    }while(input[0] != 'd' && input[0] != 'p' &&
+           input[0] != 't' && input[0] != 'd' &&
+           input[0] != 'q');
+
+    if( input == "d"){
+        cout << "Displaying your favorite pizza" << endl;
+      //  Pizza myFave("Hawaiian", 1700);
+    }
+    else if( input == "p"){
+        cout << "Make a new pizza" << endl;
+        cout << "I do nothing replace me" << endl;
+    }
+    else if( input == "t"){
+        cout << "New topping" << endl;
+        cout << "I do nothing replace me!" << endl;
+    }
+    else if( input == "s"){
+        cout << "~~Displaying pizzas~~ " << endl;
+        cout << "I do nothing, replace me!" << endl;
+    }
+    else if( input == "q"){
+        exitApplication();
+    }
 }
--- Paelingar/Petsa/Controller/AdminMenuController.h
+++ Paelingar/Petsa/Controller/AdminMenuController.h
@@ -1,16 +1,24 @@
 #ifndef ADMINMENUCONTROLLER _H
 #define ADMINMENUCONTROLLER _H
+#include <iostream>
+#include "GlobalTools.h"
+#include "GlobalView.h"
 
+using namespace std;
+using namespace GlobalTools;
+using namespace GlobalView;
 
 class AdminMenuController
 {
     public:
         AdminMenuController ();
         virtual ~AdminMenuController ();
+        void displayAdminMenu();
 
     protected:
 
     private:
+        void init();
 };
 
 #endif // ADMINMENUCONTROLLER _H
