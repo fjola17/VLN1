4c5527594f7ce26573a93ed378a116b65ea436de
halldorvalberg <34029264+halldorvalberg@users.noreply.github.com>
2017-12-15 12:05:50 +0000

    Smaller Projects Folder

---
+++ Smaller Projectes/ver2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "salary_ui.h"
+
+using namespace std;
+
+int main()
+{
+    Salary_ui ui;
+}
---
+++ Smaller Projectes/ver2/repositories/salary_repo.h
@@ -0,0 +1,26 @@
+#ifndef SALARY_REPO_H
+#define SALARY_REPO_H
+
+#include "salary_model.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Salary_repo
+{
+    public:
+        Salary_repo();
+        virtual ~Salary_repo();
+
+        void writeSalary(string nm, string ssn, double sal, int mon, int yr);
+        vector<Salary_model> readSalary();
+
+    protected:
+
+    private:
+};
+
+#endif // SALARY_REPO_H
